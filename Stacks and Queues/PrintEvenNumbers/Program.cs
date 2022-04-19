b871d80c7d8ad1583c22966bef43e714983fde21
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 15:05:52 +0300

    C#

---
+++ Stacks and Queues/PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> intQueue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Queue<int> evenQueue = new Queue<int>();
+
+            foreach (int item in intQueue)
+            {
+                if (item % 2 == 0)
+                {
+                    evenQueue.Enqueue(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",evenQueue));
+
+        }
+    }
+}
