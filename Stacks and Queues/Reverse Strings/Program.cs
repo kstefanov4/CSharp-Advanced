d4616d3f67a7c141d78384071084a0b7ae5bad48
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 12:23:37 +0300

    C#

---
+++ Stacks and Queues/Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] inputArray = Console.ReadLine().ToCharArray();
+            Stack<char> charStack = new Stack<char>();
+            foreach (var item in inputArray)
+            {
+                charStack.Push(item);
+            }
+
+            foreach (var item in charStack)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
