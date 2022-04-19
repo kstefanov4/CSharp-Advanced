bdf5b1a8899e8c043c10389e6b4e77b3129410a4
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 18:28:20 +0300

    C#

---
+++ Stacks and Queues/BasicQueueOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int numOfElements = int.Parse(input.Split()[0]);
+            int numToPop = int.Parse(input.Split()[1]);
+            int numToCheck = int.Parse(input.Split()[2]);
+
+            int[] intArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> intStack = new Queue<int>();
+
+            for (int i = 0; i < numOfElements; i++)
+            {
+                intStack.Enqueue(intArray[i]);
+            }
+
+            if (intStack.Count > 0)
+            {
+                for (int i = 0; i < numToPop; i++)
+                {
+                    intStack.Dequeue();
+                }
+            }
+
+            if (intStack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (intStack.Contains(numToCheck))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(intStack.Min());
+            }
+        }
+    }
+}
---
+++ Stacks and Queues/MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Stack<int> intStack = new Stack<int>();
+
+            for (int i = 0; i < num; i++)
+            {
+                int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                switch (array[0])
+                {
+                    case 1:
+                        intStack.Push(array[1]);
+                        break;
+
+                    case 2:
+                        intStack.Pop();
+                        break;
+                    case 3:
+                        if (intStack.Count > 0)
+                        {
+                            Console.WriteLine(intStack.Max());
+                        }
+                        break;
+                    case 4:
+                        if (intStack.Count > 0)
+                        {
+                            Console.WriteLine(intStack.Min());
+                        }
+                        break;
+                }
+                
+            }
+            Console.WriteLine(string.Join(", ", intStack));
+        }
+    }
+}
