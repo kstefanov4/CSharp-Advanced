2b7915103451587c565d74962faea5a8430b2241
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 13:44:57 +0300

    C#

---
+++ Stacks and Queues/MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<int> intStack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+                if (currentChar == '(')
+                {
+                    intStack.Push(i);
+                }
+                else if (currentChar == ')')
+                {
+                    int index = intStack.Pop();
+                    string content = input.Substring(index, i - index + 1);
+
+                    Console.WriteLine(content);
+                }
+            }
+        }
+    }
+}
