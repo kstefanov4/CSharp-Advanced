8aa7d6738dceebfce40de2464a7510a594643bb1
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 13:25:48 +0300

    C#

---
+++ Stacks and Queues/SimpleCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> inputStack = new Stack<string>(Console.ReadLine().Split().Reverse());
+           // inputStack.Reverse();
+            
+            while (inputStack.Count > 1)
+            {
+                int firstNum = int.Parse(inputStack.Pop());
+                string sighn = inputStack.Pop();
+                int secondNum = int.Parse(inputStack.Pop());
+
+                switch (sighn)
+                {
+                    case "+":
+                        inputStack.Push((firstNum + secondNum).ToString());
+                        break;
+                    case "-":
+                        inputStack.Push((firstNum - secondNum).ToString());
+                        break;
+                }
+            }
+            Console.WriteLine(inputStack.Pop());
+        }
+    }
+}
