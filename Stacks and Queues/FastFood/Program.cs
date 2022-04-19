e67a91afd392f834336303f62652c5d5d22ba3e6
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 18:54:20 +0300

    C#

---
+++ Stacks and Queues/FastFood/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            Queue<int> ordersQueue = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Console.WriteLine(ordersQueue.Max());
+
+            while (true)
+            {
+                if (ordersQueue.Count == 0 || ordersQueue.Peek() > foodQuantity)
+                {
+                    break;
+                }
+                else
+                {
+                    int currentOrder = ordersQueue.Dequeue();
+                    foodQuantity -= currentOrder;
+                }
+            }
+
+            if (ordersQueue.Any())
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", ordersQueue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
