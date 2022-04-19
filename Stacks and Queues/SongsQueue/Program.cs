579b6378f471f4cefc05ac8e8de5969e2d368b02
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 19:53:20 +0300

    C#

---
+++ Stacks and Queues/SongsQueue/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songsQueue = new Queue<string>(Console.ReadLine().Split(", "));
+
+            
+            while (songsQueue.Count > 0)
+            {
+                string[] commandArray = Console.ReadLine().Split();
+
+                switch (commandArray[0])
+                {
+                    case "Play":
+                        
+                            songsQueue.Dequeue();
+                        
+                        break;
+                    case "Add":
+                        StringBuilder sbSong = new StringBuilder();
+                        for (int i = 1; i < commandArray.Length; i++)
+                        {
+                            sbSong.Append(commandArray[i]);
+                            sbSong.Append(" ");
+                        }
+                        string song = sbSong.ToString().TrimEnd();
+
+                        if (songsQueue.Contains(song))
+                        {
+                            Console.WriteLine($"{song} is already contained!");
+                        }
+                        else
+                        {
+                            songsQueue.Enqueue(song);
+                        }
+
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songsQueue));
+                        break;
+                }
+            }
+        Console.WriteLine("No more songs!");
+        }
+    }
+}
