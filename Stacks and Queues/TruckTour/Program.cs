45f05b1eb29eda41c30887d4ea84968f9d1f4a1d
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-20 00:12:11 +0300

    C#

---
+++ Stacks and Queues/TruckTour/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TruckTour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num =int.Parse(Console.ReadLine());
+            Queue<int[]> pumpQueue = new Queue<int[]>();
+
+            for (int i = 0; i < num; i++)
+            {
+                int[] intArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                pumpQueue.Enqueue(intArray);
+            }
+
+            int index = 0;
+            int pumpIndex = 0;
+            int distance = 0;
+
+            for (int i = 0; i < pumpQueue.Count; i++)
+            {
+                int[] currentArray = pumpQueue.Peek();
+                if (currentArray[0] + distance < currentArray[1])
+                {
+                    pumpQueue.Dequeue();
+                    pumpQueue.Enqueue(currentArray);
+                    pumpIndex = index +1;
+                    i = -1;
+                    distance = 0;
+                }
+                else
+                {
+                    distance += currentArray[0] - currentArray[1];
+                    pumpQueue.Dequeue();
+                    pumpQueue.Enqueue(currentArray);
+                    index++;
+                }
+            }
+            Console.WriteLine(pumpIndex);
+        }
+    }
+}
