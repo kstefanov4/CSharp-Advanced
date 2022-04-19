9fdaf8ceed45ddc00bd96df058b49106054fb641
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 15:31:28 +0300

    C#

---
+++ Stacks and Queues/Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> peopleQueue = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    int queueLenght = peopleQueue.Count;
+
+                    for (int i = 0; i < queueLenght; i++)
+                    {
+                        Console.WriteLine(peopleQueue.Dequeue());
+                    }
+
+                }
+                else
+                {
+                    peopleQueue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{peopleQueue.Count} people remaining.");
+        }
+    }
+}
