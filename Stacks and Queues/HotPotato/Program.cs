314da35332cb4f92a38746f3c5c614675f531fa9
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 15:56:39 +0300

    C#

---
+++ Stacks and Queues/HotPotato/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> peopleQueue = new Queue<string>(Console.ReadLine().Split());
+            int turns = int.Parse(Console.ReadLine());
+
+            while (peopleQueue.Count != 1)
+            {
+                for (int i = 0; i < turns-1; i++)
+                {
+                    string name = peopleQueue.Peek();
+                    peopleQueue.Dequeue();
+                    peopleQueue.Enqueue(name);
+                }
+                Console.WriteLine($"Removed {peopleQueue.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {peopleQueue.Peek()}");
+        }
+    }
+}
