b467cc0d49f14759daa25d5c519c4a80e5897071
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-19 19:14:50 +0300

    C#

---
+++ Stacks and Queues/FashionBoutique/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothesStack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int realRackCapacity = rackCapacity;
+            int rack = 1;
+            while (clothesStack.Any())
+            {
+                if (clothesStack.Peek() > rackCapacity)
+                {
+                    rack++;
+                    rackCapacity = realRackCapacity;
+                }
+                else
+                {
+                    rackCapacity -= clothesStack.Pop();
+                }
+            }
+            Console.WriteLine(rack);
+            
+        }
+    }
+}
