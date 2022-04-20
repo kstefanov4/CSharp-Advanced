acab96e6cf3546fe39a9d140767d52ed5ca6c362
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-20 23:16:42 +0300

    C#

---
+++ Multidimensional Arrays/SumMatrixColumns/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[rowsAndCols[0], rowsAndCols[1]];
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = input[cols];
+                }
+            }
+
+            for (int cols = 0; cols < rowsAndCols[1]; cols++)
+            {
+                int sum = 0;
+                for (int rows = 0; rows < rowsAndCols[0]; rows++)
+                {
+                    sum += matrix[rows, cols];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays/SumMatrixElements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[num[0], num[1]];
+
+            for (int i = 0; i < num[0]; i++)
+            {
+                int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < num[1]; j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+
+            }
+
+            int sum = 0;
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
