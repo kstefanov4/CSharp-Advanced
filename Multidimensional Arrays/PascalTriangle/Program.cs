2a3d2f7a5c862430fa8aa40f849d7953a5bd7b36
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-21 00:58:00 +0300

    C#

---
+++ Multidimensional Arrays/PascalTriangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int[][] jaggedArray = new int[num][];
+
+            for (int i = 0; i < num; i++)
+            {
+                jaggedArray[i] = new int[i+1];
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+                jaggedArray[i][0] = 1;
+                jaggedArray[i][jaggedArray[i].Length - 1] = 1;
+
+                for (int j = 1; j < jaggedArray[i].Length-1; j++)
+                {
+                    jaggedArray[i][j] = jaggedArray[i - 1][j - 1] + jaggedArray[i - 1][j];
+                    
+                }
+            }
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(string.Join(" ",jaggedArray[i]));
+            }
+        }
+    }
+}
