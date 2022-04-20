d09d7822204e6f667843064a6a0f34c1ad79444d
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-21 00:25:02 +0300

    C#

---
+++ Multidimensional Arrays/JaggedArrayModification/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace JaggedArrayModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int[][] jaggetArray = new int[num][];
+
+            for (int i = 0; i < num; i++)
+            {
+                int[] inputNums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                jaggetArray[i] = inputNums;
+            }
+
+            string commands = Console.ReadLine();
+            while (commands != "END")
+            {
+                string[] cmdArray = commands.Split(' ');
+                int row = int.Parse(cmdArray[1]);
+                int col = int.Parse(cmdArray[2]);
+                int newValue = int.Parse(cmdArray[3]);
+
+                if (row < 0 || row > jaggetArray.Length -1 || col < 0 || col > jaggetArray[num].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                if (cmdArray[0] == "Add")
+                {
+                    jaggetArray[row][col] += newValue;
+                }
+                else if (cmdArray[0] == "Subtract")
+                {
+                    jaggetArray[row][col] -= newValue;
+                }
+
+
+                commands = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Multidimensional Arrays/SquareWithMaximumSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareWithMaximumSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[rowsAndCols[0], rowsAndCols[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputNums = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputNums[col];
+                }
+            }
+            int biggestSum = 0;
+            int biggestRow = 0;
+            int biggestCol = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = 0;
+                    sum += matrix[row, col] + matrix[row, col + 1]
+                        + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (sum > biggestSum)
+                    {
+                        biggestSum = sum;
+                        biggestRow = row;
+                        biggestCol = col;
+                    }
+                }
+
+            }
+            Console.WriteLine(matrix[biggestRow, biggestCol] + " " + matrix[biggestRow, biggestCol + 1]);
+            Console.WriteLine(matrix[biggestRow + 1, biggestCol] + " " + matrix[biggestRow + 1, biggestCol + 1]);
+            Console.WriteLine(biggestSum);
+        }
+    }
+}
