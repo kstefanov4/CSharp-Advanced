8e4434f95fe8abe2a77520d4dfcbdd2449fa9dda
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-21 13:25:00 +0300

    C#

--- Multidimensional Arrays/DiagonalDifference/Program.cs
+++ Multidimensional Arrays/DiagonalDifference/Program.cs
@@ -37,11 +37,8 @@ namespace DiagonalDifference
             int SecondaryDiagonalSum = 0;
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = matrix.GetLength(1) - 1 -i; j >= 0; j--)
-                {
-                    SecondaryDiagonalSum += matrix[i, j];
-                    break;
-                }
+                 SecondaryDiagonalSum += matrix[i, matrix.GetLength(1) - 1 -i];
+                    
             }
 
             Console.WriteLine(Math.Abs(primaryDiagonalSum- SecondaryDiagonalSum));
