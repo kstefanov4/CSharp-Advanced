1e80f7058dd8d950de611d48f6264966f7d0b957
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-21 13:47:02 +0300

    C#

---
+++ Multidimensional Arrays/2X2SquaresInMatrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _2X2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowAndCol = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            char[,] matrix = new char[rowAndCol[0], rowAndCol[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] inputChars = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputChars[col];
+                }
+            }
+            int countEqualsSquares = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        countEqualsSquares++;
+                    }
+                }
+            }
+
+            Console.WriteLine(countEqualsSquares);
+        }
+    }
+}
