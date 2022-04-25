1ae2a1d8684f9a1de224b4f687925f7b3de807ea
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-26 00:09:06 +0300

    C#

---
+++ Sets and Dictionaries Advanced/AverageStudentGrades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string input = Console.ReadLine();
+                string name = input.Split(" ")[0];
+                decimal grade = decimal.Parse(input.Split()[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<decimal>());
+                }
+                students[name].Add(grade);
+            }
+            
+
+            foreach (var student in students)
+            {
+                double averageSum = (double)student.Value.Average();
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value)} (avg: {averageSum:F2})");
+            }
+        }
+    }
+}
