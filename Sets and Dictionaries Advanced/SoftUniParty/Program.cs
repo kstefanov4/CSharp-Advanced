50c2411c21a878c9bbea2687384176f0dbfe92d1
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-26 23:18:16 +0300

    C#

---
+++ Sets and Dictionaries Advanced/SoftUniParty/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reservation = Console.ReadLine();
+            HashSet<string> set = new HashSet<string>();
+
+            while (reservation != "PARTY")
+            {
+                set.Add(reservation);
+                reservation = Console.ReadLine();
+            }
+
+            string camePeople = Console.ReadLine();
+
+            while (camePeople != "END")
+            {
+                if (set.Contains(camePeople))
+                {
+                    set.Remove(camePeople);
+                }
+                camePeople = Console.ReadLine();
+            }
+
+            HashSet<string> vip = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+
+            foreach (var number in set)
+            {
+                if (Char.IsDigit(number[0]))
+                {
+                    vip.Add(number);
+                }
+                else
+                {
+                    regular.Add(number);
+                }
+
+            }
+            int count = vip.Count + regular.Count;
+            Console.WriteLine(count);
+
+            foreach (var item in vip)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in regular)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
