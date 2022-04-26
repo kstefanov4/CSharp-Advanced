e30d8576e7233999c6d82dbcac6e06a505465418
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-04-27 00:15:55 +0300

    C#

---
+++ Sets and Dictionaries Advanced/Wardrobe/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wardrope = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] input = Console.ReadLine().Split(new string[] { " -> ", "," },StringSplitOptions.RemoveEmptyEntries);
+                string color = input[0];
+
+                if (!wardrope.ContainsKey(color))
+                {
+                    wardrope.Add(color, new Dictionary<string, int>());
+                }
+
+                for (int j = 1; j < input.Length; j++)
+                {
+                    if (!wardrope[color].ContainsKey(input[j]))
+                    {
+                        wardrope[color].Add(input[j], 0);
+                    }
+                    wardrope[color][input[j]]++;
+                }
+                
+            }
+
+            string[] serched = Console.ReadLine().Split();
+
+            foreach (var color in wardrope)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+                foreach (var cloth in color.Value)
+                {
+                    if (color.Key == serched[0] && cloth.Key == serched[1] )
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
