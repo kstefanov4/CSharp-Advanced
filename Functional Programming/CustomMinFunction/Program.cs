d517d96e77136e259f3d92103bd1485efeade8db
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-12 15:18:31 +0300

    C#

---
+++ Functional Programming/CustomMinFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int[], int> getMin = x => x.Min();
+
+            Console.WriteLine(getMin(nums));
+        }
+    }
+}
