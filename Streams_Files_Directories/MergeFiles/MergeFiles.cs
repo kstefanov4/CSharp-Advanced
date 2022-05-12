5834c1e5043b72327091dbe11e45459e6bdf867e
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-13 00:21:26 +0300

    C#

--- Streams_Files_Directories/MergeFiles/MergeFiles.cs
+++ Streams_Files_Directories/MergeFiles/MergeFiles.cs
@@ -1,6 +1,7 @@
 ﻿namespace MergeFiles
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     public class MergeFiles
     {
@@ -15,7 +16,59 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
-            
+            List<string> input1 = new List<string>();
+            List<string> input2 = new List<string>();
+
+            using (StreamReader reader = new StreamReader(firstInputFilePath))
+            {
+                string num = reader.ReadLine();
+
+                while (num != null)
+                {
+                    input1.Add(num);
+                    num = reader.ReadLine();
+                }
+            }
+
+            using (StreamReader reader = new StreamReader(secondInputFilePath))
+            {
+                string num = reader.ReadLine();
+
+                while (num != null)
+                {
+                    input2.Add(num);
+                    num = reader.ReadLine();
+                }
+            }
+
+            int minLenght = Math.Min(input1.Count, input2.Count);
+            int maxLenght = Math.Max(input1.Count, input2.Count);
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                for (int i = 0; i < minLenght; i++)
+                {
+                    writer.WriteLine(input1[i]);
+                    writer.WriteLine(input2[i]);
+                }
+
+                if (input1.Count == maxLenght)
+                {
+
+                    for (int i = minLenght; i < maxLenght; i++)
+                    {
+                        writer.WriteLine(input1[i]);
+                    }
+                }
+                else
+                {
+                    for (int i = minLenght; i < maxLenght; i++)
+                    {
+                        writer.WriteLine(input2[i]);
+                    }
+                }
+            }
+
         }
     }
 }
