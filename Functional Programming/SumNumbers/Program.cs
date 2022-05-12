3ccd4b6aaebf2d25b4e6bc191f7d2f673911813a
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-12 15:15:44 +0300

    C#

---
+++ Functional Programming/SumNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = n => int.Parse(n);
+            int[] nums = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(parser).ToArray();
+            Console.WriteLine(nums.Length);
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
