ad800524c20f4b2ebd4515b637b6eb0ba361513e
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-12 15:17:53 +0300

    C#

---
+++ Functional Programming/KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Action<string> print = x => Console.WriteLine($"Sir {x}");
+
+            foreach (var item in names)
+            {
+                print(item);
+            }
+        }
+    }
+}
