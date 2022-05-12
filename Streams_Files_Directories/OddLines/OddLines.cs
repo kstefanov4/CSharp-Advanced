bfa1cace07ae67ccc534770fe51cd55cb8f05213
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-12 23:59:42 +0300

    C#

---
+++ Streams_Files_Directories/OddLines/OddLines.cs
@@ -0,0 +1,39 @@
+﻿
+namespace OddLines
+{
+    using System;
+    using System.IO;
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputPath, outputPath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+                
+            }
+        }
+    }
+}
---
+++ Streams_Files_Directories/WordCount/WordCount.cs
@@ -0,0 +1,59 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    public class WordCount
+    {
+        static void Main(string[] args)
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> countedWords = new Dictionary<string, int>();
+            using (StreamReader reader = new StreamReader(wordsFilePath))
+            {
+                string[] words = reader.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in words)
+                {
+                    countedWords.Add(word, 0);
+                }
+            }
+
+            using (StreamReader textReader = new StreamReader(textFilePath))
+            {
+                string line = textReader.ReadLine();
+                while (line != null)
+                {
+                    List<string> wordsToCheck = line.Split(new string[] { "-", ", ", " ", "...", ".", "?", "?!" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                    foreach (string word in wordsToCheck)
+                    {
+                        string wordToCheck = word.ToLower();
+                        if (countedWords.ContainsKey(wordToCheck))
+                        {
+                            countedWords[wordToCheck]++;
+                        }
+                    }
+                    line = textReader.ReadLine();
+                }
+            }
+
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var word in countedWords.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+
+            }
+        }
+    }
+}
