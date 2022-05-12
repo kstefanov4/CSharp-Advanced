a8256889d8d74a22d2d9fe7242ec5dacce6540eb
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-12 15:16:41 +0300

    C#

---
+++ Functional Programming/AddVAT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x * 1.2)
+                .ToArray();
+
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
