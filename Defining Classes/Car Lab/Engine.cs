4c061c2538c35de3d99c2b3a0e3a79c23acf330e
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-13 16:43:27 +0300

    C#

---
+++ Defining Classes/Car Lab/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Engine
+    {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public int HorsePower { get => horsePower; set => horsePower = value; }
+        public double CubicCapacity { get => cubicCapacity; set => cubicCapacity = value; }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+    }
+}
---
+++ Defining Classes/Car Lab/Tire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public int Year { get => year; set => year = value; }
+        public double Pressure { get => pressure; set => pressure = value; }
+
+        public Tire(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+
+    }
+}
