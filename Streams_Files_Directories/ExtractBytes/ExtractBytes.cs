a81c17fe049069293ad0ca5f78e11b8745e12d70
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-13 10:10:40 +0300

    C#

---
+++ Streams_Files_Directories/ExtractBytes/ExtractBytes.cs
@@ -0,0 +1,50 @@
+﻿namespace ExtractBytes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public class ExtractBytes
+    {
+        static void Main(string[] args)
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            using (StreamReader streamReader = new StreamReader(bytesFilePath))
+            {
+                byte[] fileByte = File.ReadAllBytes(binaryFilePath);
+                List<string> byteList = new List<string>();
+                
+
+                while (!streamReader.EndOfStream)
+                {
+                    byteList.Add(streamReader.ReadLine());
+                }
+
+                foreach (var item in fileByte)
+                {
+                    if (byteList.Contains(item.ToString()))
+                    {
+                        stringBuilder.AppendLine(item.ToString());
+                    }
+
+                }
+            }
+
+            using (StreamWriter streamWriter = new StreamWriter(outputPath))
+            {
+                streamWriter.WriteLine(stringBuilder.ToString().Trim());
+            }
+
+        }
+    }
+}
