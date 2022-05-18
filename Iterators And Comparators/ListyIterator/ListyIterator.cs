563913eb5936f26600609933e4567ba302f9f1d2
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-18 15:49:11 +0300

    C#

---
+++ Iterators And Comparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        public List<T> Colection { get; set; }
+        private int index;
+        public ListyIterator(List<T> list)
+        {
+            Colection = list;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if (Colection.Count > 0)
+            {
+                Console.WriteLine(Colection[index]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            
+        }
+
+        public bool HasNext()
+        {
+            if (index + 1 < Colection.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Colection.Count; i++)
+            {
+                yield return Colection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+    }
+}
---
+++ Iterators And Comparators/ListyIterator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = new ListyIterator<string>(Console.ReadLine().Split().Skip(1).ToList());
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+                    case "Print":
+                        listyIterator.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+                    case "PrintAll":
+                        
+                            Console.WriteLine(string.Join(" ", listyIterator));
+                        
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
