cacfe9c66fc9a49ba5fb5b9d89e87de2bfb8864c
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-19 10:30:18 +0300

    C#

---
+++ Iterators And Comparators/Stack/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().Trim();
+            Stack<string> myStack = new Stack<string>();
+
+            while (input != "END")
+            {
+                string[] inputArray = input.Split(new string[] { " ", ", " }, StringSplitOptions.None);
+                string command = inputArray[0];
+
+                if (command == "Push")
+                {
+                    for (int i = 1; i < inputArray.Length; i++)
+                    {
+                        myStack.Push(inputArray[i]);
+                    }
+                }
+                else if (command == "Pop")
+                {
+                    myStack.Pop();
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
