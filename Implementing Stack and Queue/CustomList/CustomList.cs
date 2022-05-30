fbd08bb95847bb143858518e91ec77077cb38296
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-31 00:41:47 +0300

    C#

---
+++ Implementing Stack and Queue/CustomList/CustomList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList
+{
+    internal class CustomList
+    {
+        private static int defaultSize = 2;
+        private int[] items;
+        private int index;
+        public CustomList()
+        {
+            items = new int[defaultSize];
+            index = 0;
+        }
+
+        public void Add(int num)
+        {
+            if (items.Length <= index)
+            {
+                int[] newArray = Resize();
+                items = newArray;
+                items[index] = num;
+            }
+            else
+            {
+                items[index] = num;
+            }
+
+            index++;
+        }
+
+        public void RemoveAt(int num)
+        {
+            if (InRangeChecker(num))
+            {
+                Shift(num);
+                index--;
+            }
+
+            if (items.Length / 4 > index)
+            {
+                int[] newArray = Shrink();
+                items = newArray;
+            }
+        }
+
+        private int[] Shrink()
+        {
+            int[] newArray = new int[items.Length / 2];
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = items[i];
+            }
+
+            return newArray;
+        }
+
+        public bool Contains(int num)
+        {
+            for (int i = 0; i < index; i++)
+            {
+                if (items[i] == num)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Swap(int indexPosition1, int indexPosition2)
+        {
+            if (InRangeChecker(indexPosition1) && InRangeChecker(indexPosition2))
+            {
+                int temp = items[indexPosition1];
+                items[indexPosition1] = items[indexPosition2];
+                items[indexPosition2] = temp;
+            }
+            
+        }
+
+        public int Count()
+        {
+            return index;
+        }
+
+        public bool InRangeChecker(int num)
+        {
+            if (num >= 0 && num <= index)
+            {
+                return true;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(num));
+            }
+            
+        }
+
+        private int[] Resize()
+        {
+            int[] newArray = new int[items.Length * 2];
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = items[i];
+            }
+
+            return newArray;
+        }
+
+        private void Shift(int num)
+        {
+            for (int i = num; i < index - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            items[index - 1] = 0;
+        }
+
+    }
+}
--- Implementing Stack and Queue/CustomList/Program.cs
+++ Implementing Stack and Queue/CustomList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CustomList
 {
@@ -6,7 +7,41 @@ namespace CustomList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CustomList customList = new CustomList();
+            customList.Add(10);
+            customList.Add(20);
+            customList.Add(30);
+            // 10, 20, 30
+            customList.RemoveAt(1);
+            // 10, 30
+            customList.Add(40);
+            // 10, 30, 40
+            customList.RemoveAt(0);
+            //30,40
+        //    customList.RemoveAt(5);
+            //IndexOutOfRangeException
+            customList.Contains(30);
+            //true
+            customList.Contains(500);
+            //false
+            customList.Add(100);
+            //30,40,100
+            customList.Swap(0, 2);
+            //100,40,30
+         //   customList.Swap(0, 5);
+            //IndexOutOfRangeException
+            int count = customList.Count();
+            customList.Add(40);
+            customList.Add(50);
+            customList.Add(60);
+            //100,40,30,40,50,60
+            customList.RemoveAt(0);
+            customList.RemoveAt(0);
+            customList.RemoveAt(0);
+            customList.RemoveAt(0);
+            customList.RemoveAt(0);
+            //60
+            //Shrink to 4 lenght
         }
     }
 }
