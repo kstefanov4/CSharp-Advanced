df1361982dde2f75b04c3e872f5ecd1f159ed31e
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-30 23:20:43 +0300

    C#

---
+++ Implementing Stack and Queue/CustomList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
