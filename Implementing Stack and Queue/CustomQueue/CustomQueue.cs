f3245fe60a431512b403c256df038267020eaa8e
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-05-31 13:51:29 +0300

    C#

---
+++ Implementing Stack and Queue/CustomQueue/CustomQueue.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomQueue
+{
+    class CustomQueue : IEnumerable
+    {
+        private const int defaultSize = 4;
+        private int[] items;
+        private int index;
+        private int zero;
+
+        public CustomQueue()
+        {
+            this.items = new int[defaultSize];
+            index = 0;
+            zero = items[index];
+        }
+
+        public void Enqueue(int input)
+        {
+            if (items.Length <= index)
+            {
+                int[] newArray = Resize();
+                items = newArray;
+            }
+
+            items[index] = input;
+            index++;
+        }
+
+        public int Dequeue()
+        {
+            if (items.Length > 0)
+            {
+                int numToReturn = items[0];
+                for (int i = 0; i < index-1; i++)
+                {
+                    items[i] = items[i + 1];
+                }
+                items[index - 1] = zero;
+                index--;
+                CheckForShrink();
+                return numToReturn;
+
+            }
+            else
+            {
+                throw new InvalidOperationException("Custom Stack is empty");
+            }
+
+        }
+        public int Peek()
+        {
+            if (items.Length > 0)
+            {
+                return items[index - 1];
+            }
+            else
+            {
+                throw new InvalidOperationException("Custom Stack is empty");
+            }
+
+        }
+
+        public void Clear()
+        {
+            for (int i = index - 1; i >= 0; i--)
+            {
+                items[i] = zero;
+                index--;
+            }
+
+            CheckForShrink();
+        }
+
+        public int Count()
+        {
+            return index;
+        }
+
+
+        private int[] Resize()
+        {
+            int[] newArray = new int[items.Length * 2];
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = items[i];
+            }
+
+            return newArray;
+        }
+
+        private int[] Shrink()
+        {
+            int[] newArray = new int[items.Length / 2];
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = items[i];
+            }
+
+            return newArray;
+        }
+
+        private void CheckForShrink()
+        {
+            if (items.Length / 4 > index)
+            {
+                int[] newArray = Shrink();
+                items = newArray;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < index; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+       
+    }
+}
---
+++ Implementing Stack and Queue/CustomQueue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CustomQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue customQueue = new CustomQueue();
+            customQueue.Enqueue(10);
+            customQueue.Enqueue(20);
+            customQueue.Enqueue(30);
+            customQueue.Enqueue(40);
+            customQueue.Enqueue(50);
+            //10,20,30,40,50 - array lenght = 8
+            Console.WriteLine(customQueue.Dequeue());
+            Console.WriteLine(customQueue.Dequeue());
+            //10,20,30
+            Console.WriteLine(customQueue.Peek());
+            // return 30
+            customQueue.Clear();
+            // clear all elements
+            customQueue.Enqueue(10);
+            customQueue.Enqueue(20);
+            customQueue.Enqueue(30);
+            customQueue.Enqueue(40);
+            customQueue.Enqueue(50);
+            //
+            Console.WriteLine(customQueue.Count());
+            // return 5
+            foreach (var item in customQueue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
