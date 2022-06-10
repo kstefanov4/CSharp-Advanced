ab7fe4109271260557f1433660cbba6c0e4bb0df
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-06-10 15:46:32 +0300

    C#

--- Exams/FoodFinder2ndVariant/Program.cs
+++ Exams/FoodFinder2ndVariant/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,6 +7,7 @@ namespace FoodFinder
 
     class Food
     {
+        
         private List<char> letters;
         public Food(string word)
         {
@@ -16,12 +17,14 @@ namespace FoodFinder
         public string Name { get; }
         public List<char> Letters { get => letters; set => letters = value; }
 
-        public void RemoveFromList(char letterToCheck)
+        public bool Contains(char letterToCheck)
         {
             if (Letters.Contains(letterToCheck))
             {
                 Letters.Remove(letterToCheck);
+                return true;
             }
+            return false;
         }
     }
     class Program
@@ -29,15 +32,7 @@ namespace FoodFinder
         private static List<Food> foodList;
         static void Main(string[] args)
         {
-            Dictionary<string, List<char>> foods = new Dictionary<string, List<char>>
-            {
-                {"pear", new List<char>("pear".ToList()) },
-                {"flour", new List<char>("flour".ToList()) },
-                {"pork", new List<char>("pork".ToList()) },
-                {"olive", new List<char>("olive".ToList()) }
-
-            };
-
+            
             foodList = new List<Food>();
             Food pear = new Food("pear");
             Food flour = new Food("flour");
@@ -55,42 +50,19 @@ namespace FoodFinder
             while (consonants.Count > 0)
             {
                 char vowelsLetter = vowels.Dequeue();
-                foreach (var item in foods)
-                {
-                    if (item.Value.Contains(vowelsLetter))
-                    {
-                        item.Value.Remove(vowelsLetter);
-                    }
-                }
-               // CheckLetter(vowelsLetter);
+                CheckLetter(vowelsLetter);
                 vowels.Enqueue(vowelsLetter);
 
                 char consonantsLetters = consonants.Pop();
-                foreach (var item in foods)
-                {
-                    if (item.Value.Contains(consonantsLetters))
-                    {
-                        item.Value.Remove(consonantsLetters);
-                    }
-                }
-                // CheckLetter(consonantsLetters);
+                CheckLetter(consonantsLetters);
             }
 
-            Dictionary<string, List<char>> filteredFood = new Dictionary<string, List<char>>();
+            foodList = foodList.Where(x => x.Letters.Count == 0).ToList();
 
-            foreach (var food in foods)
+            Console.WriteLine($"Words found: {foodList.Count}");
+            foreach (var food in foodList)
             {
-                if (food.Value.Count == 0)
-                {
-                    filteredFood.Add(food.Key,food.Value);
-                }
-            }
-            //foodList = foodList.Where(x => x.Letters.Count == 0).ToList();
-
-            Console.WriteLine($"Words found: {filteredFood.Count}");
-            foreach (var food in filteredFood)
-            {
-                Console.WriteLine(food.Key);
+                Console.WriteLine(food.Name);
             }
         }
 
@@ -98,7 +70,7 @@ namespace FoodFinder
         {
             foreach (var food in foodList)
             {
-                food.RemoveFromList(vowelsLetter);
+                food.Contains(vowelsLetter);
             }
         }
     }
