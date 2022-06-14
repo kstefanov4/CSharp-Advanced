91591606ac208f208cba44be59b6ea1bdcbd6018
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-06-14 09:46:09 +0300

    C#

--- Exams/StockMarket/Investor.cs
+++ Exams/StockMarket/Investor.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace StockMarket
 {
@@ -6,57 +8,85 @@ namespace StockMarket
     {
         private ICollection<Stock> portfolio;
 
-        public Investor (string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
-	    {
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
             FullName = fullName;
             EmailAddress = emailAddress;
             MoneyToInvest = moneyToInvest;
             BrokerName = brokerName;
             Portfolio = new List<Stock>();
-	    }
+        }
 
         public string FullName { get; set; }
         public string EmailAddress { get; set; }
         public decimal MoneyToInvest { get; set; }
         public string BrokerName { get; set; }
-        public Icollection<Stock> Portfolio { get => portfolio; set => portfolio = value; }
+        public ICollection<Stock> Portfolio { get => portfolio; set => portfolio = value; }
 
         public int Count => portfolio.Count;
 
         public void BuyStock(Stock stock)
         {
-            
-            if (stock.MarketCapitalization > 10000 && MoneyToInvest >= stock.MarketCapitalization && !portfolio.Contains(stock))
-	        {
+
+            if (stock.MarketCapitalization > 10000 && MoneyToInvest >= stock.PricePerShare && !portfolio.Contains(stock))
+            {
                 portfolio.Add(stock);
-                MoneyToInvest -= stock.PricePerSharer;
-	        }
-	        
+                MoneyToInvest -= stock.PricePerShare;
+            }
+
         }
 
         public string SellStock(string companyName, decimal sellPrice)
         {
-            Stock stock = Portfolio.FirsOrDefault(x => x.Name == companyName).FirstOrDefault();
-            if (stock != null)
-	        {
+            Stock stock = Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+            if (stock == null)
+            {
                 return $"{companyName} does not exist.";
-	        }
+            }
             else
-	        {
+            {
                 if (sellPrice < stock.PricePerShare)
-	            {
+                {
                     return $"Cannot sell {companyName}.";
-	            }
+                }
 
                 Portfolio.Remove(stock);
                 MoneyToInvest += sellPrice;
-                return $"{companyName} was sold."
+                return $"{companyName} was sold.";
 
-	        }
+
+            }
         }
         public Stock FindStock(string companyName)
         {
-            Stock stock =
+            return Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+        }
+
+        public Stock FindBiggestCompany()
+        {
+            if (Portfolio.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return Portfolio.OrderByDescending(x => x.MarketCapitalization).First();
+            }
+
+        }
+
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks: ");
+
+            foreach (Stock stock in Portfolio)
+            {
+                sb.Append(stock);
+                sb.AppendLine();
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
