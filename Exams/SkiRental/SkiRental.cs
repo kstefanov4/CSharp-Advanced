a511575899accb1373d759a82b59e8d96a34fe9f
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-06-23 16:56:35 +0300

    C#

---
+++ Exams/SkiRental/SkiRental.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SkiRental
+{
+    class SkiRental
+    {
+        private List<Ski> data;
+
+        public SkiRental(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.data = new List<Ski>();
+        }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public void Add(Ski ski)
+        {
+
+            if (data.Count + 1 <= Capacity )
+            {
+                data.Add(ski);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            //data.RemoveAll(x => x.Manufacturer == manufacturer && x.Model == model);
+            
+            Ski ski = data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+            if (ski != null)
+            {
+                data.Remove(ski);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Ski GetNewestSki()
+        {
+            return data.OrderByDescending(x => x.Year).FirstOrDefault();
+        }
+
+        public Ski GetSki(string manufacturer, string model)
+        {
+            return data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+        }
+
+        public int Count => data.Count;
+
+        public string GetStatistics()
+        {
+            return $"The skis stored in {Name}:" + Environment.NewLine +
+            string.Join(Environment.NewLine, data);
+        }
+    }
+}
