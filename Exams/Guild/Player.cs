8c989f8a695f37f19dc919525f5f6d238ef391f3
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-06-24 20:49:54 +0300

    C#

---
+++ Exams/Guild/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    class Player
+    {
+        
+        public Player(string name, string playerClass)
+        {
+            this.Name = name;
+            this.Class = playerClass;
+            this.Rank = "Trial";
+            this.Description = "n/a";
+        }
+        public string Name{ get; set; }
+        public string Class{ get; set; }
+        public string Rank{ get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Player {Name}: {Class}" + Environment.NewLine +
+                $"Rank: {Rank}" + Environment.NewLine +
+                $"Description: {Description}";
+        }
+    }
+}
