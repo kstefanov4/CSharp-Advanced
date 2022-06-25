b769f770001890b4bf3303908953e65912d3af34
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-06-25 12:17:14 +0300

    C#

---
+++ Exams/Renovators/Catalog.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+        private List<Renovator> renovators;
+
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            this.Name = name;
+            this.NeededRenovators = neededRenovators;
+            this.Project = project;
+            this.renovators = new List<Renovator>();
+        }
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+
+        public int Count => renovators.Count;
+
+        public string AddRenovator(Renovator renovator)
+        {
+            if (renovators.Count + 1 > NeededRenovators)
+            {
+                return "Renovators are no more needed.";
+            }
+
+            if (string.IsNullOrEmpty(renovator.Name) || string.IsNullOrEmpty(renovator.Type))
+            {
+                return "Invalid renovator's information.";
+            }
+
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+
+            renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog.";
+            
+        }
+
+        public bool RemoveRenovator(string name)
+        {
+            Renovator renovator = renovators.FirstOrDefault(x => x.Name == name);
+            if (renovator != null)
+            {
+                renovators.Remove(renovator);
+                return true;
+            }
+            return false;
+        }
+
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            int count = 0;
+
+            foreach (var renovator in renovators)
+            {
+                if (renovator.Type == type)
+                {
+                    count++;
+                }
+            }
+
+            renovators.RemoveAll(x => x.Type == type);
+
+            return count;
+        }
+
+        public Renovator HireRenovator(string name)
+        {
+            foreach (var renovator in renovators)
+            {
+                if (renovator.Name == name)
+                {
+                    renovator.Hired = true;
+                    return renovator;
+                }
+            }
+            return null;
+        }
+
+        public List<Renovator> PayRenovators(int days)
+        {
+            List<Renovator> payRenovators = new List<Renovator>(renovators.Where(x => x.Days >= days));
+            return payRenovators;
+        }
+
+        public string Report()
+        {
+            List<Renovator> notHiredRenovators = new List<Renovator>(renovators.Where(x => x.Hired == false));
+
+            return $"Renovators available for Project {Project}" + Environment.NewLine +
+                string.Join(Environment.NewLine, notHiredRenovators);
+        }
+    }
+}
---
+++ Exams/Renovators/Renovator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Rate = rate;
+            this.Days = days;
+            this.Hired = false;
+        }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Rate { get; set; }
+        public int Days { get; set; }
+        public bool Hired { get; set; }
+
+        public override string ToString()
+        {
+            return $"-Renovator: {Name}" + Environment.NewLine +
+                $"--Specialty: {Type}" + Environment.NewLine +
+                $"--Rate per day: {Rate} BGN";
+        }
+    }
+}
